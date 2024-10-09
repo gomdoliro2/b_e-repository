0ed07c148f8a12d8357b12ce4411e3df10e2c25a
hsm <hll2071@naver.com>
2024-10-09 21:39:12 +0900

    도메인 전체 허요 인증 X

--- src/main/java/gomdoliro/gomdol/Config/WebConfig.java
+++ src/main/java/gomdoliro/gomdol/Config/WebConfig.java
@@ -8,10 +8,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001") // 허용할 도메인 설정
+                .allowedOrigins("*") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 허용할 헤더 설정
-                .allowCredentials(true) // 인증정보 허용 여부
                 .maxAge(3600); // preflight 요청의 유효시간 설정
     }
 }
