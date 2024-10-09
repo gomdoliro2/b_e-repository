e5a4ad399038ee4bcee3ef0feb5fe0c0748a82b3
hsm <hll2071@naver.com>
2024-10-09 21:46:20 +0900

    롤백

--- src/main/java/gomdoliro/gomdol/Config/WebConfig.java
+++ src/main/java/gomdoliro/gomdol/Config/WebConfig.java
@@ -1,16 +1,19 @@
 package gomdoliro.gomdol.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 허용할 도메인 설정
+                .allowedOrigins("http://localhost:3001") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 허용할 헤더 설정
+                .allowCredentials(true) // 인증정보 허용 여부
                 .maxAge(3600); // preflight 요청의 유효시간 설정
     }
 }
