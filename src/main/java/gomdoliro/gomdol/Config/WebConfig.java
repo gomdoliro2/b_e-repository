728a2753d8b1a718b983ec1f8458fd900cb8aece
hsm <hll2071@naver.com>
2024-10-09 21:31:49 +0900

    CORS 인증정보 임시 비활성화

--- src/main/java/gomdoliro/gomdol/Config/WebConfig.java
+++ src/main/java/gomdoliro/gomdol/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3001") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 허용할 헤더 설정
-                .allowCredentials(true) // 인증정보 허용 여부
+                //.allowCredentials(true) // 인증정보 허용 여부
                 .maxAge(3600); // preflight 요청의 유효시간 설정
     }
 }
